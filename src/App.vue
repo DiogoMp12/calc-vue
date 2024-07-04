959c329d8642bcce76dcb7ef1ba7813f3d5097e0
Diogo Peres <plaer654@gmail.com>
2024-07-04 14:09:23 -0300

    finalização

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <div class="row">
       <div class="col-md-6 offset-md-3">
         <h1 class="text-center my-4">Calculadora Aritmética</h1>
-        <form @submit.prevent="calcularResultado">
+        <form>
           <div class="mb-3">
             <label for="numero1" class="form-label">Primeiro Número</label>
-            <input type="number" required class="form-control" id="numero1" v-model.number="numero1">
+            <input type="number" class="form-control" id="numero1" v-model.number="numero1">
           </div>
           <div class="mb-3">
             <label for="numero2" class="form-label">Segundo Número</label>
-            <input type="number" required class="form-control" id="numero2" v-model.number="numero2">
+            <input type="number" class="form-control" id="numero2" v-model.number="numero2">
           </div>
           <div class="mb-3">
             <label for="operacao" class="form-label">Operação</label>
@@ -21,56 +21,39 @@
               <option value="divisao">Divisão (/)</option>
             </select>
           </div>
-          <button type="submit" class="btn btn-primary w-100" style="border-radius: 50px;">Calcular</button>
         </form>
-        <h3 class="text-center mt-4" v-if="resultadoVisivel">Resultado: {{ resultado }}</h3>
+        <h3 class="text-center mt-4">Resultado: {{ resultado }}</h3>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
-      numero1: null,
-      numero2: null,
-      operacao: 'soma',
-      resultado: null,
-      resultadoVisivel: false
+      numero1: 0,
+      numero2: 0,
+      operacao: 'soma'
     };
   },
-  methods: {
-    calcularResultado() {
-      if (this.numero1 === null || this.numero2 === null) {
-        alert('Por favor, preencha todos os campos.');
-        return;
-      }
-      
-      let res;
+  computed: {
+    resultado() {
       switch (this.operacao) {
         case 'soma':
-          res = this.numero1 + this.numero2;
-          break;
+          return this.numero1 + this.numero2;
         case 'subtracao':
-          res = this.numero1 - this.numero2;
-          break;
+          return this.numero1 - this.numero2;
         case 'multiplicacao':
-          res = this.numero1 * this.numero2;
-          break;
+          return this.numero1 * this.numero2;
         case 'divisao':
-          res = this.numero2 !== 0 ? this.numero1 / this.numero2 : 'Erro: Divisão por zero';
-          break;
+          return this.numero2 !== 0 ? this.numero1 / this.numero2 : 'Erro: Divisão por zero';
         default:
-          res = 0;
+          return 0;
       }
-      this.resultado = res;
-      this.resultadoVisivel = true;
     }
   }
 };
 </script>
-
 <style scoped>
 h1, h3 {
   color: #0059ff;
